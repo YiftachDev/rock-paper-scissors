877cf6367d3ab806200d70dd7470fa32c19b92f3
YiftachDev <yiftach007@gmail.com>
2023-02-11 16:21:10 +0200

    Created function that handels the computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Test")
+function getComputerChoice() {
+    let choice = Math.round(Math.random() * 2);
+    if (choice == 0) choice = "Rock";
+    else if (choice == 1) choice = "Paper";
+    else choice = "Scissors";
+    return choice;
+}

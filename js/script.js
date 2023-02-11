0e7314e1d7c7f1e746ab4e8139509f6733728df7
YiftachDev <yiftach007@gmail.com>
2023-02-11 16:23:17 +0200

    change the computer choice function - choice to be all lowercase

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 function getComputerChoice() {
     let choice = Math.round(Math.random() * 2);
-    if (choice == 0) choice = "Rock";
-    else if (choice == 1) choice = "Paper";
-    else choice = "Scissors";
+    if (choice == 0) choice = "rock";
+    else if (choice == 1) choice = "paper";
+    else choice = "scissors";
     return choice;
+}
+
+function getPlayerChoice() {
+    let choice = prompt("Rock Paper or Scissors?");
+    return choice.toLocaleLowerCase();
 }

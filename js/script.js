d4063bcbf65c81e1f39ed7b0984d2524a8698b7a
YiftachDev <yiftach007@gmail.com>
2023-02-11 16:32:49 +0200

    Create check win function and created scores veriables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     let choice = Math.round(Math.random() * 2);
     if (choice == 0) choice = "rock";
@@ -9,4 +13,16 @@ function getComputerChoice() {
 function getPlayerChoice() {
     let choice = prompt("Rock Paper or Scissors?");
     return choice.toLocaleLowerCase();
+}
+
+function checkWin(playerChoice, computerChoice) {
+    if (PlayerChoice == computerChoice) {
+        return "tie";
+    } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")) {
+        playerScore++;
+        return "win";
+    } else if ((computerChoice == "rock" && playerChoice == "scissors") || (computerChoice == "paper" && playerChoice == "rock") || (computerChoice == "scissors" && playerChoice == "paper")) {
+        computerScore++;
+        return "lose";
+    }
 }
